1ca7065f89d767cd8dd982c58cc4a9cb2bdf5a91
Вероника <69638667+veronikaa-kuznetsova@users.noreply.github.com>
2024-05-15 10:20:36 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './app/App'
+import { Router } from 'react-router-dom'
+import { createStore } from './app/store/createStore'
+import { Provider } from 'react-redux'
+import history from './app/utils/history'
+
+const store = createStore()
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
